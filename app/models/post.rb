ed64a53a747beba12de8c9f087423994b03a569f
Riley Moore <rilemoore@gmail.com>
2019-12-17 23:12:05 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
-class Post < ActiveRecord::Base
+class ClickbaitValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
+      record.errors.add attribute, "Not a clickbait title"
+    end
+  end
 end
+
+class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, clickbait: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  
+end
+
